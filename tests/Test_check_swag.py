57bec01ce7edb2da9e3420d3e3d6e34a2257987e
MariaBeng <mashaskl@mail.ru>
2024-06-12 00:16:19 +0300

    Third Commit

--- tests/Test_check_swag.py
+++ tests/Test_check_swag.py
@@ -4,8 +4,19 @@ def test_check_swag(browser):
     demo_qa_page = SwagLabs(browser)
     demo_qa_page.visit()
     demo_qa_page.exist_icon()
-    demo_qa_page.exist_username()
-    demo_qa_page.exist_password()
     assert demo_qa_page.exist_icon()
-    assert demo_qa_page.exist_username()
-    assert demo_qa_page.exist_password()
+
+def test_check_username(browser):
+     demo_qa_page = SwagLabs(browser)
+     demo_qa_page.visit()
+     demo_qa_page.exist_username()
+     assert demo_qa_page.exist_username()
+
+def test_check_password(browser):
+     demo_qa_page = SwagLabs(browser)
+     demo_qa_page.visit()
+     demo_qa_page.exist_password()
+     assert demo_qa_page.exist_password()
+
+
+
